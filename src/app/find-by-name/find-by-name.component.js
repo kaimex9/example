5f6e2530e4a696d0e18cef57eabceb477199f0b7
kaimex9 <kaimexalabau@gmail.com>
2024-12-01 23:49:20 +0100

    finished

--- src/app/find-by-name/find-by-name.component.js
+++
@@ -1,9 +0,0 @@
-function nurses() {
-    $jsonData = [{"user":"Emmeline","password":"iM5}~tp/"},
-    {"user":"Susana","password":"wP7@bQp@|S~HlhI"},
-    {"user":"Aharon","password":"zE4)U\'ptR"},
-    {"user":"Ardath","password":"eE3/}$}Fh5"},
-    {"user":"Cyrill","password":"pQ7?\'1+$<l"}];
-    
-    return json_decode($jsonData, true); // Corrección aquí
-}
--- src/app/find-by-name/find-by-name.component.ts
+++ src/app/find-by-name/find-by-name.component.ts
@@ -1,12 +1,31 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-find-by-name',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgFor],
   templateUrl: './find-by-name.component.html',
   styleUrl: './find-by-name.component.css'
 })
 export class FindByNameComponent {
+  nurseName: string = ''; // Para almacenar el valor del input
+  nursesList = [
+    { user: "Emmeline", password: "iM5}~tp/" },
+    { user: "Susana", password: "zE4)U'ptR" },
+    { user: "Susana", password: "pQ7?'1+$<l" },
+    { user: "Susana", password: "wP7@bQp@|S~HlhI" },
+    { user: "Aharon", password: "zE4)U'ptR" },
+    { user: "Ardath", password: "eE3/}$}Fh5" },
+    { user: "Cyrill", password: "pQ7?'1+$<l" }
+  ];
+  filteredNurses: any[] = []; // Resultado filtrado
 
+  search() {
+    this.filteredNurses = this.nursesList.filter(nurse =>
+      nurse.user.toLowerCase().includes(this.nurseName.toLowerCase())
+    );
+  }
 }
